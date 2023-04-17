229b63029fae61845424ffb724139a4745f07487
siddhardik <siddharthabhunia2001@gmail.com>
2023-04-17 21:28:01 +0530

    JS

--- index.js
+++ index.js
@@ -2,30 +2,96 @@
 
 const time=document.querySelector('.show-time');
 
-// buttons 
-const start=document.getElementById('start');
-const stop=document.getElementById('stop');
-const reset=document.getElementById('reset');
+// // buttons 
+const start_btn=document.getElementById("start");
+const stop_btn=document.getElementById("stop");
+const reset_btn=document.getElementById("reset");
 
-// time.innerHTML='05 : 00 : 00 : 00';
 
-let ds=36610;
+start_btn.addEventListener("click",start);
+stop_btn.addEventListener("click",stop);
+reset_btn.addEventListener("click",reset);
+
+
+var ds=0;
+let cds=0;
 let ss=0;
 let mm=0;
 let hh=0;
-hh=Math.floor(ds/36000);
-    mm=Math.floor((ds-hh*36000)/600);
-    ss=Math.floor((ds-hh*36000-mm*600)/10);
-    ds=ds-hh*36000-mm*600-ss*10;
+// hh=Math.floor(ds/36000);
+//     mm=Math.floor((ds-hh*36000)/600);
+//     ss=Math.floor((ds-hh*36000-mm*600)/10);
+//     ds=ds-hh*36000-mm*600-ss*10;
+//     if(ds<10){
+//         ds="0"+ds;
+//     }
+//     if(ss<10){
+//         ss="0"+ss;
+//     }
+//      if(mm<10){
+//         mm="0"+mm;
+//     }
+//     if(hh<10){
+//         hh="0"+hh;
+//     }
+//     time.innerText=`${hh} : ${mm} : ${ss} : ${ds}`;
+    // time.inneText=samay;
 
-    let samay=`${hh} : ${mm} : ${ss} : ${ds}`;
-    time.inneText=samay;
-
-function timer(){
+function timeIncrease(){
     ds++;
     
-    hh=Math.floor(ds/36000);
-    mm=Math.floor((ds-hh*36000)/600);
+    cds=ds%10;
     ss=Math.floor((ds-hh*36000-mm*600)/10);
-    ss=ds-hh*36000-mm*600-ss*10;
+    mm=Math.floor((ds-hh*36000)/600);
+    hh=Math.floor(ds/36000);
+
+    if(cds<10){
+        cds="0"+cds;
+    }
+    if(ss<10){
+        ss="0"+ss;
+    }
+     if(mm<10){
+        mm="0"+mm;
+    }
+    if(hh<10){
+        hh="0"+hh;
+    }
+
+    time.innerText=`${hh} : ${mm} : ${ss} : ${cds}`;  
+}
+
+
+let interval =undefined;
+
+function start(){
+    if(interval==undefined){
+        interval=setInterval(timeIncrease,100);
+        // 100 miliseconds = 1 desisecond 
+    }
+    else{
+        return;
+    }
+
+}
+
+function stop(){
+    if(interval!=undefined){
+         // StopWatch is on running state
+         clearInterval(interval);
+         interval=undefined;
+    }
+    else{
+      alert("Stopwatch is Off Now! ⌚");
+    }
+}
+
+function reset(){
+    alert("Are you sure you want to reset🤔?")
+    stop();
+    ds="00",hh="00",mm="00",ss="00";
+
+    time.innerText=`${hh} : ${mm} : ${ss} : ${ds}`;
+
+
 }
